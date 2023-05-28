6692ede6bc9534af64b1de88825254ca8be94c2d
clarence_c <clarence@biteline.net>
2023-05-28 13:45:18 -0700

    create routes / init controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  editThought,
+  deleteThought,
+  addReaction, 
+  removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// TODO: don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+// /api/thoughts/:thoughtID
+router.route('/:thoughtId').get(getSingleThought).put(editThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtID/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  editUser,
+  deleteUser,
+  addFriend, 
+  removeFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(editUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
